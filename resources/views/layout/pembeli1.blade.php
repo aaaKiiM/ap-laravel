64e31d44ee0f0f99c0a0804a4215cad1b70b3ff0
Kim <108006977+aaaKiiM@users.noreply.github.com>
2022-08-27 21:56:34 +0700

    Test

--- resources/views/layout/pembeli1.blade.php
+++ resources/views/layout/pembeli1.blade.php
@@ -64,7 +64,7 @@
                 <div class="row">
                     <div class="col-md-12 col-lg-5">
                         <div class="header-top-welcome">
-                            <p>Selamat Datang Di Website Emperor Cake!</p>
+                            <p>Selamat Datang </p>
                         </div>
                     </div>
                     <div class="col-md-5 col-lg-3">
