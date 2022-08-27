c0cbda4a3119c791ce9edb809bbd993b33f817f0
Kim <108006977+aaaKiiM@users.noreply.github.com>
2022-08-27 22:04:52 +0700

    Test 2

--- resources/views/layout/pembeli1.blade.php
+++ resources/views/layout/pembeli1.blade.php
@@ -64,7 +64,7 @@
                 <div class="row">
                     <div class="col-md-12 col-lg-5">
                         <div class="header-top-welcome">
-                            <p>Selamat Datang </p>
+                            <p>Selamat Datang DI Test</p>
                         </div>
                     </div>
                     <div class="col-md-5 col-lg-3">
